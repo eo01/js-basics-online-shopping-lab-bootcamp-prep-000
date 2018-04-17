551bc97ed4e0343f35dc78201cf0b8347434720d
eo 01 <eo_01@protonmail.com>
2018-04-17 16:45:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.floor(Math.random()*100) + 1;
- var item_object = {};
- item_object[item] = price
- cart.push({[item]:price})
- console.log(`${item} has been added to your cart.`);
- return getCart()
+ var price = Math.random() * 100
+ var roundedPrice = Math.floor(price)
+ cart.push({[item]: roundedPrice})
+ console.log(item + " has been added to your cart.")
+ return cart;
 }
 
 function viewCart() {
